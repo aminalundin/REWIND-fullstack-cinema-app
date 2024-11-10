f47790528ff4e5e8df79954bdd17784ef62b7a1d
aminalundin <lundinamina1@gmail.com>
2024-11-10 20:29:06 +0100

    created server with express

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
+// dependencies
 import express from 'express';
+
+// variables
+const PORT = 3000;
+
+// express environment
+const app = express();
+
+// route - listen to requests
+app.get('/', (req, res) => {
+    res.send("Hello! it worked");
+});
+
+// start server
+app.listen(PORT, () => {
+    console.log(`listening on port: ${PORT}`);
+});
