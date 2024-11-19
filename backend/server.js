73833e05d648f22f0780f56303f40920f0deebd2
aminalundin <lundinamina1@gmail.com>
2024-11-19 18:41:47 +0100

    installed mongoose, cors and dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,17 @@
-// dependencies
 import express from 'express';
 
-// variables
+
 const PORT = 3000;
 
-// express environment
+
 const app = express();
 
-// route - listen to requests
+
 app.get('/', (req, res) => {
     res.send("Hello! it worked");
 });
 
-// start server
+
 app.listen(PORT, () => {
     console.log(`listening on port: ${PORT}`);
-});
+})
