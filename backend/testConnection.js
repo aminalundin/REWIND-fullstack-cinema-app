5b15c2619a6c33d6ff463db72d8e5477e8bb147a
aminalundin <lundinamina1@gmail.com>
2024-11-21 13:26:28 +0100

    testing connection

--- backend/testConnection.js
+++ backend/testConnection.js
@@ -3,9 +3,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const MONGO_URI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
+console.log(MONGO_URI)
+
+const uri = "mongodb+srv://lundinamina:hejmongoDB-@amina-1.sl8n8.mongodb.net/?retryWrites=true&w=majority&appName=Amina-1";
 
 mongoose
-  .connect(MONGO_URI)
+  .connect(uri)
   .then(() => {
     console.log('Connected to database');
    
