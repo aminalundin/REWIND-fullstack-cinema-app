48947f80a11170e9de713d594269ab891e1cefed
aminalundin <lundinamina1@gmail.com>
2024-11-27 17:50:43 +0100

    updated movie schema

--- backend/models/Movies.js
+++ backend/models/Movies.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
 
-// const mongoose = require('mongoose');
-
 const movieSchema = new mongoose.Schema({
-  title: String,
- 
+  title: { type: String, required: true },
+  director: { type: String, required: true },
+  duration: { type: String, required: true }, 
+  genre: { type: String, required: true },
+  posterUrl: { type: String, required: true }
 });
 
 const Movie = mongoose.model('movies', movieSchema);
